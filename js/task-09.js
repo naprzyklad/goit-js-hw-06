a8368fec087f11529be5b4abaaa4f6c5ed917efd
naprzyklad <mk18@spoko.pl>
2024-01-22 19:45:00 +0100

    Update 2

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const colorSpan = document.querySelector(".color");
+  const colorButton = document.querySelector(".change-color");
+
+  colorButton.addEventListener("click", () => {
+    const newColor = getRandomHexColor();
+    document.body.style.backgroundColor = newColor;
+    colorSpan.textContent = newColor;
+  });
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+});
